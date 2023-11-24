df13d72e0378eda30a2674ccd5ec08d1d9ead316
NT15MCP <46614988+nt15mcp@users.noreply.github.com>
2023-11-24 17:32:46 -0500

    Remove sensitive data

---
+++ includes/dbh.inc.php
@@ -0,0 +1,13 @@
+<?php
+
+$ini = parse_ini_file('/home/u437830502/domains/wmipl.net/Configs/app.ini');
+$servername = $ini['db_server'];
+$dbusername = $ini['db_user'];
+$dbpassword = $ini['db_pword'];
+$database = $ini['db_name'];
+
+$conn = mysqli_connect($servername, $dbusername, $dbpassword, $database);
+
+if(!$conn){
+	die("Connection failed: ".mysqli_connect_error());
+	}
