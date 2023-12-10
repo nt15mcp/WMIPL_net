b9f7287bdc1b22f013afc44292d13876652c3e10
NT15MCP <46614988+nt15mcp@users.noreply.github.com>
2023-12-10 16:02:36 -0500

    Update database table and column names

--- includes/edit.inc.php
+++ includes/edit.inc.php
@@ -2,7 +2,7 @@
 session_start();
 if(isset($_POST['edit-profile'])){
 	require "dbh.inc.php";
-	$sql = "SELECT * FROM User WHERE LoginID=".$_SESSION['userID'];
+	$sql = "SELECT * FROM userd WHERE login_id=".$_SESSION['userID'];
 	$stmt = mysqli_stmt_init($conn);
 	if(!mysqli_stmt_prepare($stmt, $sql)){
 		header("Location: ../".$_SESSION['page'].".php?error=sqlierror");
